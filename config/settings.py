4e57ce09f2339000551ffcf6ff28023b6c88cce0
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2021-06-28 16:06:07 +0000

    Fix DEBUG in production

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DJANGO_DEBUG"]
+DEBUG = os.environ["DJANGO_DEBUG"] == "true"
 
 ALLOWED_HOSTS = []
 
