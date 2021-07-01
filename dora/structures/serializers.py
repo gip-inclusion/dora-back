be6a1e718d32b7fce313bc65043c75aedbe292bc
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2021-07-01 08:08:24 +0000

    Serialize verbose solution types

--- dora/structures/serializers.py
+++ dora/structures/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 
-from .models import Structure
+from .models import SolutionThemes, Structure
 
 
 class StructureSerializer(serializers.ModelSerializer):
+    solutions_themes = serializers.SerializerMethodField()
+
     class Meta:
         model = Structure
         # Temporary, while working on the exact model content
         fields = "__all__"
+
+    def get_solutions_themes(self, obj):
+        def get_choice_label(choices, value):
+            return [l for [v, l] in choices if v == value][0]
+
+        return [
+            get_choice_label(SolutionThemes.choices, value)
+            for value in obj.solutions_themes
+        ]
