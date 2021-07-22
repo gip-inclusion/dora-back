876efeb8e6550b86d21b2d95f82bcae0283bf976
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2021-07-22 14:52:54 +0200

    Now import all SIRENE data

--- dora/sirene/management/commands/import_sirene.py
+++ dora/sirene/management/commands/import_sirene.py
@@ -1,8 +1,10 @@
 import csv
+import os.path
 import pathlib
 import subprocess
 import tempfile
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from django.db.utils import DataError
@@ -24,55 +26,74 @@ def get_full_search_text_value(row, parent_name):
      """
 
 
+USE_TEMP_DIR = not settings.DEBUG
+
+
+def commit(rows):
+    Establishment.objects.bulk_create(rows)
+
+
 class Command(BaseCommand):
     help = "Import the latest Sirene database"
 
     def handle(self, *args, **options):
         with tempfile.TemporaryDirectory() as tmp_dir_name:
-            the_dir = pathlib.Path(tmp_dir_name)
-            print(tmp_dir_name)
+            if USE_TEMP_DIR:
+                the_dir = pathlib.Path(tmp_dir_name)
+            else:
+                the_dir = pathlib.Path("/tmp")
+            self.stdout.write("Saving SIRENE files to " + str(the_dir))
 
             legal_units_file_url = (
                 "https://files.data.gouv.fr/insee-sirene/StockUniteLegale_utf8.zip"
             )
             zipped_stock_file = the_dir / "StockUniteLegale_utf8.zip"
 
-            self.stdout.write(self.style.NOTICE("Downloading legal units file"))
-            subprocess.run(
-                ["curl", legal_units_file_url, "-o", zipped_stock_file],
-                check=True,
-            )
+            if not os.path.exists(zipped_stock_file):
+                self.stdout.write(self.style.NOTICE("Downloading legal units file"))
+                subprocess.run(
+                    ["curl", legal_units_file_url, "-o", zipped_stock_file],
+                    check=True,
+                )
 
-            self.stdout.write(self.style.NOTICE("Unzipping legal units file"))
-            subprocess.run(
-                ["unzip", zipped_stock_file, "-d", the_dir],
-                check=True,
-            )
+                self.stdout.write(self.style.NOTICE("Unzipping legal units file"))
+                subprocess.run(
+                    ["unzip", zipped_stock_file, "-d", the_dir],
+                    check=True,
+                )
 
             stock_file = the_dir / "StockUniteLegale_utf8.csv"
 
             establishments_geo_file_url = "https://data.cquest.org/geo_sirene/v2019/last/StockEtablissementActif_utf8_geo.csv.gz"
             gzipped_estab_file = the_dir / "StockEtablissementActif_utf8_geo.csv.gz"
 
-            self.stdout.write(self.style.NOTICE("Downloading establishments file"))
-            subprocess.run(
-                ["curl", establishments_geo_file_url, "-o", gzipped_estab_file],
-                check=True,
-            )
+            if not os.path.exists(gzipped_estab_file):
+                self.stdout.write(self.style.NOTICE("Downloading establishments file"))
+                subprocess.run(
+                    ["curl", establishments_geo_file_url, "-o", gzipped_estab_file],
+                    check=True,
+                )
 
-            self.stdout.write(self.style.NOTICE("Unzipping establishments file"))
-            subprocess.run(
-                ["gzip", "-d", gzipped_estab_file],
-                check=True,
-            )
+                self.stdout.write(self.style.NOTICE("Unzipping establishments file"))
+                subprocess.run(
+                    ["gzip", "-dk", gzipped_estab_file],
+                    check=True,
+                )
 
             estab_file = the_dir / "StockEtablissementActif_utf8_geo.csv"
 
+            num_stock_items = 0
+            with open(stock_file) as f:
+                num_stock_items = sum(1 for line in f)
             with open(stock_file) as units_file:
                 units_reader = csv.DictReader(units_file, delimiter=",")
                 units = {}
                 self.stdout.write(self.style.NOTICE("Parsing legal units"))
-                for row in units_reader:
+                for i, row in enumerate(units_reader):
+                    if (i % 1_000_000) == 0:
+                        self.stdout.write(
+                            self.style.NOTICE(f"{round(100*i/num_stock_items)}% done")
+                        )
                     assert row["statutDiffusionUniteLegale"] == "O"
                     if row["denominationUniteLegale"]:
                         units[row["siren"]] = {
@@ -80,27 +101,35 @@ class Command(BaseCommand):
                             "diffusable": row["statutDiffusionUniteLegale"],
                             "sigle": row["sigleUniteLegale"],
                         }
+                num_establishments = 0
+                with open(estab_file) as f:
+                    num_establishments = sum(1 for line in f)
                 with open(estab_file) as establishment_file:
                     reader = csv.DictReader(establishment_file, delimiter=",")
                     self.stdout.write(self.style.NOTICE("Importing establishments"))
                     with transaction.atomic(durable=True):
                         Establishment.objects.all().delete()
-                        for row in reader:
+                        batch_size = 10_000
+                        rows = []
+                        for i, row in enumerate(reader):
+                            if (i % batch_size) == 0:
+                                self.stdout.write(
+                                    self.style.NOTICE(
+                                        f"{round(100*i/num_establishments)}% done"
+                                    )
+                                )
+                                commit(rows)
+                                rows = []
                             try:
                                 assert row["statutDiffusionEtablissement"] == "O"
                                 code_commune = row["codeCommuneEtablissement"]
-                                if (
-                                    code_commune.startswith("974")
-                                    or code_commune.startswith("08")
-                                    or code_commune.startswith("44")
-                                ):
-                                    siren = row["siren"]
-                                    parent = units.get(siren)
-                                    parent_name = (
-                                        parent["denomination"] if parent else ""
-                                    )
 
-                                    Establishment.objects.create(
+                                siren = row["siren"]
+                                parent = units.get(siren)
+                                parent_name = parent["denomination"] if parent else ""
+
+                                rows.append(
+                                    Establishment(
                                         siren=siren,
                                         siret=row["siret"],
                                         denomination=row[
@@ -151,8 +180,11 @@ class Command(BaseCommand):
                                         if row["latitude"]
                                         else None,
                                     )
+                                )
 
                             except DataError as err:
-                                print(err, row)
+                                self.stdout.write(self.style.ERROR(err))
+                                self.stdout.write(self.style.ERROR(row))
+                        commit(rows)
 
                 self.stdout.write(self.style.SUCCESS("Import successful"))
