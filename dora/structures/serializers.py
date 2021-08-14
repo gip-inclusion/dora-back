8f5e1dc474d81764e7c047606e6862ffa3b331ce
Cedric Rossi <cedric@terramater.net>
2021-08-14 18:13:07 +0200

    Add typology display name to Structure serializer

--- dora/structures/serializers.py
+++ dora/structures/serializers.py
@@ -4,6 +4,8 @@ from .models import Structure
 
 
 class StructureSerializer(serializers.ModelSerializer):
+    typology_display = serializers.CharField(source="get_typology_display")
+
     class Meta:
         model = Structure
         # Temporary, while working on the exact model content
