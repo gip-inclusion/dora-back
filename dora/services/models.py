507aab6c232662d6d226f04764d97816709e41d6
Cedric Rossi <cedric@terramater.net>
2021-08-16 19:54:42 +0200

    Precise Services required fields

--- dora/services/models.py
+++ dora/services/models.py
@@ -29,14 +29,14 @@ class BeneficiaryAccessMode(models.TextChoices):
     ONSITE = ("OS", "Se présenter")
     PHONE = ("PH", "Téléphoner")
     EMAIL = ("EM", "Envoyer un mail")
-    OTHER = ("OT", "Autre")
+    OTHER = ("OT", "Autre (préciser)")
 
 
 class CoachOrientationMode(models.TextChoices):
     PHONE = ("PH", "Téléphoner")
     EMAIL = ("EM", "Envoyer un mail")
     EMAIL_PRESCRIPTION = ("EP", "Envoyer un mail avec une fiche de prescription")
-    OTHER = ("OT", "Autre")
+    OTHER = ("OT", "Autre (préciser)")
 
 
 class LocationKind(models.TextChoices):
@@ -85,7 +85,10 @@ class Service(models.Model):
     ##############
     # Presentation
     name = models.CharField(verbose_name="Nom de l’offre", max_length=140)
-    short_desc = models.TextField(verbose_name="Résumé", max_length=280, blank=True)
+    short_desc = models.TextField(
+        verbose_name="Résumé",
+        max_length=280,
+    )
     full_desc = models.TextField(
         verbose_name="Descriptif complet de l’offre", blank=True
     )
@@ -95,22 +98,16 @@ class Service(models.Model):
     kinds = ArrayField(
         models.CharField(max_length=2, choices=ServiceKind.choices),
         verbose_name="Type de service",
-        blank=True,
-        default=list,
         db_index=True,
     )
     categories = ArrayField(
         models.CharField(max_length=2, choices=ServiceCategories.choices),
         verbose_name="Catégorie principale",
-        blank=True,
-        default=list,
         db_index=True,
     )
     subcategories = ArrayField(
         models.CharField(max_length=3, choices=ServiceSubCategories.choices),
         verbose_name="Sous-catégorie",
-        blank=True,
-        default=list,
     )
     is_common_law = models.BooleanField(
         verbose_name="Il s’agit d'un service de Droit commun ?", default=False
@@ -139,8 +136,6 @@ class Service(models.Model):
     beneficiaries_access_modes = ArrayField(
         models.CharField(max_length=2, choices=BeneficiaryAccessMode.choices),
         verbose_name="Comment mobiliser la solution en tant que bénéficiaire",
-        blank=True,
-        default=list,
     )
     beneficiaries_access_modes_other = CharField(
         verbose_name="Autre", max_length=280, blank=True
@@ -148,8 +143,6 @@ class Service(models.Model):
     coach_orientation_modes = ArrayField(
         models.CharField(max_length=2, choices=CoachOrientationMode.choices),
         verbose_name="Comment orienter un bénéficiaire en tant qu’accompagnateur",
-        blank=True,
-        default=list,
     )
     coach_orientation_modes_other = CharField(
         verbose_name="Autre", max_length=280, blank=True
@@ -182,13 +175,16 @@ class Service(models.Model):
     # Contact
 
     contact_name = models.CharField(
-        max_length=140, verbose_name="Nom du contact référent", blank=True
+        max_length=140,
+        verbose_name="Nom du contact référent",
     )
     contact_phone = models.CharField(
-        verbose_name="Numéro de téléphone", max_length=10, blank=True
+        verbose_name="Numéro de téléphone",
+        max_length=10,
     )
-    contact_email = models.EmailField(verbose_name="Courriel", blank=True)
-    contact_url = models.URLField(verbose_name="Site web", blank=True)
+    contact_email = models.EmailField(
+        verbose_name="Courriel",
+    )
     is_contact_info_public = models.BooleanField(
         verbose_name="Rendre mes informations publiques",
         default=False,
@@ -199,18 +195,25 @@ class Service(models.Model):
     location_kind = ArrayField(
         models.CharField(max_length=2, choices=LocationKind.choices),
         verbose_name="Lieu de déroulement",
-        blank=True,
-        default=list,
     )
 
     remote_url = models.URLField(verbose_name="Lien visioconférence", blank=True)
-    address1 = models.CharField(verbose_name="Adresse", max_length=255, blank=True)
+    address1 = models.CharField(
+        verbose_name="Adresse",
+        max_length=255,
+    )
     address2 = models.CharField(
         verbose_name="Compléments d’adresse", max_length=255, blank=True
     )
-    postal_code = models.CharField(verbose_name="Code postal", max_length=5, blank=True)
+    postal_code = models.CharField(
+        verbose_name="Code postal",
+        max_length=5,
+    )
     city_code = models.CharField(verbose_name="Code INSEE", max_length=5, blank=True)
-    city = models.CharField(verbose_name="Ville", max_length=200, blank=True)
+    city = models.CharField(
+        verbose_name="Ville",
+        max_length=200,
+    )
     longitude = models.FloatField(blank=True, null=True)
     latitude = models.FloatField(blank=True, null=True)
 
