55292aa62f25d1ac4e64a661f8da6b798401e4c4
Cedric Rossi <cedric@terramater.net>
2021-08-19 10:56:26 +0200

    More details in Structure admin listing

--- dora/structures/admin.py
+++ dora/structures/admin.py
@@ -4,7 +4,13 @@ from .models import Structure
 
 
 class StructureAdmin(admin.ModelAdmin):
-    list_display = ["name", "slug"]
+    list_display = [
+        "name",
+        "slug",
+        "modification_date",
+        "last_editor",
+    ]
+    ordering = ["-modification_date"]
 
 
 admin.site.register(Structure, StructureAdmin)
