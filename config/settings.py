5ae0ef3b06c59d3e0a73bf93efb840890238eb4c
Cedric Rossi <cedric@terramater.net>
2021-08-20 10:33:52 +0200

    Stop sending sentry alerts from dev env

--- config/settings.py
+++ config/settings.py
@@ -17,18 +17,19 @@ import sentry_sdk
 from corsheaders.defaults import default_headers
 from sentry_sdk.integrations.django import DjangoIntegration
 
-sentry_sdk.init(
-    dsn=os.environ["SENTRY_DSN"],
-    integrations=[DjangoIntegration()],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-    environment=os.environ["SENTRY_ENVIRONMENT"],
-)
+if os.environ["ENVIRONMENT"] != "local":
+    sentry_sdk.init(
+        dsn=os.environ["SENTRY_DSN"],
+        integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+        environment=os.environ["ENVIRONMENT"],
+    )
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
