4bba5d194d170ecbf477ea091087dc75c6dee03a
Cedric Rossi <cedric@terramater.net>
2021-09-27 16:35:58 +0200

    Disable mattermost notification on dev environments

--- dora/core/notify.py
+++ dora/core/notify.py
@@ -6,14 +6,18 @@ from django.conf import settings
 
 def send_mattermost_notification(msg):
     try:
-        if settings.MATTERMOST_HOOK_KEY:
-            requests.post(
-                url=f"https://mattermost.incubateur.net/hooks/{settings.MATTERMOST_HOOK_KEY}",
-                headers={
-                    "Content-Type": "application/json",
-                },
-                data=json.dumps({"text": msg}),
-            )
+        if not settings.ENVIRONMENT == "local":
+            if settings.MATTERMOST_HOOK_KEY:
+                requests.post(
+                    url=f"https://mattermost.incubateur.net/hooks/{settings.MATTERMOST_HOOK_KEY}",
+                    headers={
+                        "Content-Type": "application/json",
+                    },
+                    data=json.dumps({"text": msg}),
+                )
+        else:
+            print(msg)
+
     except requests.exceptions.RequestException:
         # TODO: logging
         print("HTTP Request failed")
