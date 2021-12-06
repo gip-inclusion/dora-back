e92152f77c740a1573379518f7ecbe4050bb50e0
Cedric Rossi <cedric@terramater.net>
2021-12-06 17:13:17 +0100

    Superuser can create service in any structure

--- dora/structures/views.py
+++ dora/structures/views.py
@@ -48,7 +48,7 @@ class StructureViewSet(
         user = self.request.user
         only_mine = self.request.query_params.get("mine")
 
-        if only_mine:
+        if only_mine and not user.is_staff:
             return (
                 self.get_my_structures(user).order_by("-modification_date").distinct()
             )
