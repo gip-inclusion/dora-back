f7ed17dcf892152f00702c9d09228e22c76dcc7a
Cedric Rossi <cedric@terramater.net>
2022-02-24 11:50:32 +0100

    Beautify output

--- dora/api/views.py
+++ dora/api/views.py
@@ -1,3 +1,4 @@
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer
 from rest_framework import permissions, viewsets
 
 from dora.services.models import Service
@@ -6,11 +7,19 @@ from dora.structures.models import Structure
 from .serializers import ServiceSerializer, StructureSerializer
 
 
+class PrettyCamelCaseJSONRenderer(CamelCaseJSONRenderer):
+    def render(self, data, media_type=None, renderer_context=None):
+        renderer_context = renderer_context or {}
+        renderer_context["indent"] = 4
+        return super().render(data, media_type, renderer_context)
+
+
 class StructureViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Structure.objects.all()
     serializer_class = StructureSerializer
     permission_classes = [permissions.AllowAny]
+    renderer_classes = [PrettyCamelCaseJSONRenderer]
 
 
 class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
@@ -18,3 +27,5 @@ class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Service.objects.filter(is_draft=False, is_suggestion=False)
     serializer_class = ServiceSerializer
     permission_classes = [permissions.AllowAny]
+
+    renderer_classes = [PrettyCamelCaseJSONRenderer]
