69400d608a73720816256fbe5f7a7b9cbc26e634
vmttn <taq@protonmail.ch>
2022-03-31 20:14:56 +0200

    feat: ajout de commandes d'import itou (#17)

--- dora/core/utils.py
+++ dora/core/utils.py
@@ -1,2 +1,18 @@
+from typing import Tuple
+
+from django.utils.text import Truncator
+
+
+def normalize_description(desc: str, limit: int) -> Tuple[str, str]:
+    if len(desc) < limit:
+        return desc, ""
+    else:
+        return Truncator(desc).chars(limit), desc
+
+
+def normalize_phone_number(phone: str) -> str:
+    return "".join([c for c in phone if c.isdigit()])[:10]
+
+
 def code_insee_to_code_dept(code_insee):
     return code_insee[:3] if code_insee.startswith("97") else code_insee[:2]
