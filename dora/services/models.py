2114c01005f52ff07adc7660f035e1ae256b187b
Alexandre CANTIN <cantin.alexandre@gmail.com>
2022-07-13 12:04:49 +0200

    feat(services): cumulation des temps de contribution d'un service (#51)

--- dora/services/models.py
+++ dora/services/models.py
@@ -298,6 +298,10 @@ class Service(models.Model):
     modification_date = models.DateTimeField(auto_now=True)
     publication_date = models.DateTimeField(blank=True, null=True)
 
+    # Temps passé (en seconde) sur le formulaire de création d'un service - avant la *toute* première publication
+    # Plus exactement : temps de contribution cumulé en brouillon + temps de contribution final menant au statut "publié"
+    filling_duration = models.IntegerField(null=True, default=None)
+
     creator = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True
     )
