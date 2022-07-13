3dd62be793505c616cc1a9384c85fa207e360aa7
Cedric Rossi <cedric@terramater.net>
2022-07-13 12:20:45 +0200

    Corrige un conflit de migration

--- dora/services/migrations/0059_service_last_draft_notification_date.py
+++ dora/services/migrations/0059_service_last_draft_notification_date.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("services", "0057_service_filling_duration"),
+        ("services", "0058_alter_service_filling_duration"),
     ]
 
     operations = [
