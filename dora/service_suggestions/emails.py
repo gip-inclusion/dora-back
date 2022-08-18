1eb63b7855fb8806bb43d9f4df4cc6c88e22f971
AlexandreCantin <cantin.alexandre@gmail.com>
2022-08-18 17:04:51 +0200

    Gestion des structures existantes mais sans admin

--- dora/service_suggestions/emails.py
+++ dora/service_suggestions/emails.py
@@ -4,13 +4,13 @@ from django.template.loader import render_to_string
 from dora.core.emails import send_mail
 
 
-def send_suggestion_validated_new_structure_email(email, structure):
+def send_suggestion_validated_welcome_email(email, structure):
     params = {
         "structure": structure,
         "cta_link": f"{settings.FRONTEND_URL}/auth/inscription?siret={structure.siret}",
         "homepage_url": settings.FRONTEND_URL,
     }
-    body = render_to_string("new_structure.html", params)
+    body = render_to_string("suggestion_validated_welcome.html", params)
 
     send_mail(
         "[DORA] Des acteurs de l’insertion sont intéressés par vos services !",
@@ -20,13 +20,13 @@ def send_suggestion_validated_new_structure_email(email, structure):
     )
 
 
-def send_suggestion_validated_existing_structure_email(to, structure, service):
+def send_suggestion_validated_structure_admin_email(to, structure, service):
     params = {
         "structure": structure,
         "cta_link": service.get_frontend_url(),
         # "more_details_link": "",
     }
-    body = render_to_string("existing_structure.html", params)
+    body = render_to_string("suggestion_validated_admin.html", params)
 
     send_mail(
         "[DORA] Vous avez reçu une nouvelle suggestion de service ! 🥳 🎉",
