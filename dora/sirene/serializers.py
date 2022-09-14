c9c17153b66ee665ef53ad53037a34d2c4c36f9c
Cedric Rossi <cedric@terramater.net>
2022-09-14 19:58:59 +0200

    Ajoute une info PE lors de la recherche de siret

--- dora/sirene/serializers.py
+++ dora/sirene/serializers.py
@@ -4,6 +4,8 @@ from .models import Establishment
 
 
 class EstablishmentSerializer(serializers.ModelSerializer):
+    is_pole_emploi = serializers.SerializerMethodField()
+
     class Meta:
         model = Establishment
         fields = [
@@ -19,4 +21,14 @@ class EstablishmentSerializer(serializers.ModelSerializer):
             "siren",
             "siret",
             "is_siege",
+            "is_pole_emploi",
         ]
+
+    def get_is_pole_emploi(self, obj):
+        from dora.structures.models import Structure, StructureTypology
+
+        try:
+            structure = Structure.objects.get(siret=obj.siret)
+            return structure.typology == StructureTypology.objects.get(value="PE")
+        except Structure.DoesNotExist:
+            return False
