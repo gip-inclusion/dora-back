5a246781bb7480c888b82345bc8198cffc08f32a
Cedric Rossi <cedric@terramater.net>
2022-09-15 18:03:31 +0200

    Nettoyage

--- dora/sirene/serializers.py
+++ dora/sirene/serializers.py
@@ -4,8 +4,6 @@ from .models import Establishment
 
 
 class EstablishmentSerializer(serializers.ModelSerializer):
-    is_pole_emploi = serializers.SerializerMethodField()
-
     class Meta:
         model = Establishment
         fields = [
@@ -21,14 +19,4 @@ class EstablishmentSerializer(serializers.ModelSerializer):
             "siren",
             "siret",
             "is_siege",
-            "is_pole_emploi",
         ]
-
-    def get_is_pole_emploi(self, obj):
-        from dora.structures.models import Structure, StructureTypology
-
-        try:
-            structure = Structure.objects.get(siret=obj.siret)
-            return structure.typology == StructureTypology.objects.get(value="PE")
-        except Structure.DoesNotExist:
-            return False
