2f0ae9357510e0d346de37d9dc359a701d24576d
Cedric Rossi <cedric@terramater.net>
2022-10-20 13:01:43 +0200

    WIP

--- dora/structures/management/commands/import_data_inclusion.py
+++ dora/structures/management/commands/import_data_inclusion.py
@@ -13,138 +13,149 @@ from dora.sirene.models import Establishment
 from dora.structures.models import Structure, StructureSource, StructureTypology
 from dora.users.models import User
 
-# SORTED_SOURCES = ["mes-aides"]
-SORTED_SOURCES = ["itou"]  # , "siao", "cd72", "cd35"]
+# Documentation DI : https://data-inclusion-api-prod.osc-secnum-fr1.scalingo.io/api/v0/docs
 
 BASE_URL = furl(settings.DATA_INCLUSION_URL)
 
-
-def get_results_page(url, page_number):
-    paginated_url = url.copy().add({"page": page_number})
-    print(paginated_url)
-    response = requests.get(
-        paginated_url,
-        params={},
-        headers={
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": f"Bearer {settings.DATA_INCLUSION_API_KEY}",
-        },
-    )
-    print(
-        "Response HTTP Status Code: {status_code}".format(
-            status_code=response.status_code
-        )
-    )
-    return json.loads(response.content)["items"]
-
-
-def get_structures(source):
-    structures = []
-    page = 1
-
-    url = BASE_URL.add(
-        path="structures/",
-        args={"source": source, "size": 100, "departement": "08"},
-    )
-
-    while True:
-        page_structures = get_results_page(url, page)
-        if len(page_structures):
-            structures += page_structures
-        else:
-            break
-        page += 1
-    return structures
+# On importe les structures inexistantes source par source, dans l'ordre.
+SORTED_SOURCES = ["itou"]  # , "siao", "cd72", "cd35"]
 
 
 class Command(BaseCommand):
-    # TODO
-    help = ""
+    help = "Importe les nouvelles structures Inclusion Connect qui n'existent pas encore dans Dora"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--department", type=str)
+        parser.add_argument("structures_slugs", nargs="*")
 
     def handle(self, *args, **options):
+        department = options["department"]
         for source in SORTED_SOURCES:
-
-            self.stdout.write(f"Importing: {source}")
+            self.stdout.write(self.style.SUCCESS(f"Import de la source: {source}"))
             try:
-                structures = get_structures(source)
-            except requests.exceptions.RequestException:
-                print("HTTP Request failed")
-                break
-            # import pprint
-            #
-            # pprint.pprint(structures)
-            self.import_structures(source, structures)
+                structures = self.get_structures(source, department)
+                self.import_structures(source, structures)
+            except requests.exceptions.RequestException as e:
+                self.stderr.write(self.style.ERROR(e))
 
-    def import_structures(self, source, structures):
-        # for structure in structures:
-        #     try:
-        #         s = Structure.objects.get(siret=structure["siret"])
-        #         self.stdout.write(f"Deleting: {s.name}")
-        #         s.delete()
-        #     except Structure.DoesNotExist:
-        #         pass
+    def get_structures(self, source, department):
+        structures = []
+        args = {"source": source, "size": 100}
+        if department:
+            args["departement"] = department
+
+        url = BASE_URL.add(
+            path="structures/",
+            args=args,
+        )
+
+        for results in self.get_pages(url):
+            structures += results
+
+        return structures
+
+    def get_pages(self, url):
+        page = 1
+        while True:
+            paginated_url = url.copy().add({"page": page})
+            self.stdout.write(f"Chargement de {url}")
+            response = requests.get(
+                paginated_url,
+                params={},
+                headers={
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": f"Bearer {settings.DATA_INCLUSION_API_KEY}",
+                },
+            )
+            if response.status_code != 200:
+                self.stderr.write(
+                    self.style.ERROR(
+                        f"Erreur dans la récupération des données\n{url}: {response.status_code}"
+                    )
+                )
+                return
+
+            result = json.loads(response.content)["items"]
+            if len(result):
+                yield result
+                page += 1
+            else:
+                return
+
+    def import_structures(self, source_value, structures):
+        # # TODO DON'T COMMIT THIS CODE
+        # if True:
+        #     for structure in structures:
+        #         try:
+        #             s = Structure.objects.get(siret=structure["siret"])
+        #             s.delete()
+        #         except Structure.DoesNotExist:
+        #             pass
+        # ########################
 
         bot_user = User.objects.get_dora_bot()
-
-        existing_structures_sirets = set(
-            Structure.objects.all().values_list("siret", flat=True)
+        source, _created = StructureSource.objects.get_or_create(
+            value=f"di-{source_value}",
+            defaults={"label": "source_value"},
         )
-        structures_to_import = [
-            s for s in structures if s["siret"] not in existing_structures_sirets
-        ]
 
-        for di_structure in structures_to_import:
-            if di_structure["structure_parente"]:
-                print(di_structure)
-            continue
-            self.stdout.write(f"Importing: {di_structure['nom']}")
-            return
-            try:
-                establishment = Establishment.objects.get(siret=di_structure["siret"])
-            except Establishment.DoesNotExist:
-                self.stdout.write(f"Siret incorrect: {di_structure['siret']}")
+        for s in structures:
+            if Structure.objects.filter(siret=s["siret"]).exists():
                 continue
+            if s["structure_parente"]:
+                continue
+            try:
+                establishment = Establishment.objects.get(siret=s["siret"])
+            except Establishment.DoesNotExist:
+                self.stdout.write(
+                    self.style.NOTICE(
+                        f"Siret incorrect, ignoré : {s['siret']} ({s['nom']})"
+                    )
+                )
+                continue
+            try:
+                typology, _created = StructureTypology.objects.get_or_create(
+                    value=s["typologie"]
+                )
+                structure = Structure.objects.create(
+                    siret=s["siret"],
+                    name=s["nom"] or establishment.name,
+                    address1=s["adresse"] or establishment.address1,
+                    address2=s["complement_adresse"] or "",
+                    city_code=s["code_insee"] or establishment.city_code,
+                    postal_code=s["code_postal"] or establishment.postal_code,
+                    city=s["commune"] or establishment.city,
+                    latitude=s["latitude"] or establishment.latitude,
+                    longitude=s["longitude"] or establishment.longitude,
+                    email=s["courriel"] or "",
+                    phone=utils.normalize_phone_number(s["telephone"] or ""),
+                    url=s["site_web"] or "",
+                    full_desc=s["presentation_detail"] or "",
+                    short_desc=s["presentation_resume"] or "",
+                    typology=typology,
+                    opening_hours=s["horaires_ouverture"],
+                    other_labels=",".join(s["labels_autres"]),
+                    accesslibre_url=s["accessibilite"],
+                    # parent=s['structure_parente'],
+                    ape=establishment.ape,
+                    source=source,
+                    creator=bot_user,
+                    last_editor=bot_user,
+                    modification_date=timezone.now(),
+                )
 
-            typology, _created = StructureTypology.objects.get_or_create(
-                value=di_structure["typologie"]
-            )
-            source, _created = StructureSource.objects.get_or_create(
-                value=f"di-{source}",
-                defaults={"label": f"Data Inclusion ({source})"},
-            )
-            structure = Structure.objects.create(
-                siret=di_structure["siret"],
-                name=di_structure["nom"] or establishment.name,
-                address1=di_structure["adresse"] or establishment.address1,
-                address2=di_structure["complement_adresse"] or "",
-                city_code=di_structure["code_insee"] or establishment.city_code,
-                postal_code=di_structure["code_postal"] or establishment.postal_code,
-                city=di_structure["commune"] or establishment.city,
-                latitude=di_structure["latitude"] or establishment.latitude,
-                longitude=di_structure["longitude"] or establishment.longitude,
-                email=di_structure["courriel"] or "",
-                phone=utils.normalize_phone_number(di_structure["telephone"] or ""),
-                url=di_structure["site_web"] or "",
-                full_desc=di_structure["presentation_detail"] or "",
-                short_desc=di_structure["presentation_resume"] or "",
-                typology=typology,
-                opening_hours=di_structure["horaires_ouverture"],
-                other_labels=di_structure["labels_autres"],
-                accesslibre_url=di_structure["accessibilite"],
-                # parent=di_structure['structure_parente'],
-                ape=establishment.ape,
-                source=source,
-                creator=bot_user,
-                last_editor=bot_user,
-                modification_date=timezone.now(),
-            )
+                structure.national_labels.set(s["labels_nationaux"])
 
-            structure.national_labels.set(di_structure["labels_nationaux"])
-
-            send_moderation_notification(
-                structure,
-                bot_user,
-                f"Structure importée de Data Inclusion ({source})",
-                ModerationStatus.VALIDATED,
-            )
+                send_moderation_notification(
+                    structure,
+                    bot_user,
+                    f"Structure importée de Data Inclusion ({source})",
+                    ModerationStatus.VALIDATED,
+                )
+                self.stdout.write(
+                    f"Importé : {structure.name}, {structure.get_frontend_url()}"
+                )
+            except Exception as e:
+                print(s)
+                self.stderr.write(self.style.ERROR(e))
