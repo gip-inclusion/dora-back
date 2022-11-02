b811feb768b9ee9912150419735253fc2f07eac0
Cedric Rossi <cedric@terramater.net>
2022-11-02 15:14:00 +0100

    Corrige la recherche par siret dans l'admin

--- dora/sirene/admin.py
+++ dora/sirene/admin.py
@@ -4,8 +4,14 @@ from .models import Establishment
 
 
 class EstablishmentAdmin(admin.ModelAdmin):
+    show_full_result_count = False
     list_display = ["name", "siren", "siret", "city", "city_code"]
-    search_fields = ("full_search_text", "siret")
+    search_help_text = "Recherche par nom, siren, ou siret"
+    search_fields = (
+        "siret__exact",
+        "siren__exact",
+        "full_search_text",
+    )
 
 
 admin.site.register(Establishment, EstablishmentAdmin)
