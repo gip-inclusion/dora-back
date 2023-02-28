5c6acdb1e14366dd198aa0df91b1d2f48251aaa2
AlexandreCantin <cantin.alexandre@gmail.com>
2023-02-28 11:17:58 +0100

    Ordre des migrations

--- dora/users/migrations/0015_user_extra_infos.py
+++ dora/users/migrations/0015_user_extra_infos.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("users", "0013_user_last_notification_email_sent"),
+        ("users", "0014_user_department_user_is_local_coordinator"),
     ]
 
     operations = [
