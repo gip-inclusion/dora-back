f4b73ea57d3f4558dbf36ff296cdbd921f962f82
Alexandre CANTIN <cantin.alexandre@gmail.com>
2023-03-13 18:31:35 +0100

    Ajout du guide de démarrage (#107)

--- dora/rest_auth/serializers.py
+++ dora/rest_auth/serializers.py
@@ -41,6 +41,7 @@ class UserInfoSerializer(serializers.ModelSerializer):
             "phone_number",
             "short_name",
             "structures",
+            "onboarding_actions_accomplished",
             "token_expiration",
         ]
 
