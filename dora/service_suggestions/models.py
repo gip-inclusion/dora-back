59b49cb8055e77e2cd632b2da2bc6b6ded2d32fa
AlexandreCantin <cantin.alexandre@gmail.com>
2023-04-13 16:16:23 +0200

    Better syntax

--- dora/service_suggestions/models.py
+++ dora/service_suggestions/models.py
@@ -72,11 +72,7 @@ class ServiceSuggestion(models.Model):
 
     def convert_to_service(self, send_notification_mail=False, user=None):
         def values_to_objects(Model, values):
-            if Model in [ServiceSubCategory, ServiceCategory]:
-                # On ignore les thématiques et les besoins qui n'existeraient plus
-                models = [Model.objects.filter(value=v).first() for v in values]
-                return [m for m in models if m]
-            return [Model.objects.get(value=v) for v in values]
+            return Model.objects.filter(value__in=values)
 
         is_new_structure = False
         try:
