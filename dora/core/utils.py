cd64fa9dc56339d93610f46d6f98e529eb234153
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-10-04 19:14:59 +0200

    Onboarding v2

--- dora/core/utils.py
+++ dora/core/utils.py
@@ -2,12 +2,9 @@ import logging
 import re
 from typing import Tuple
 
-import sib_api_v3_sdk
-from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
 from django.utils.text import Truncator
-from sib_api_v3_sdk.rest import ApiException as SibApiException
 
 logger = logging.getLogger(__name__)
 
@@ -36,35 +33,6 @@ def normalize_phone_number(phone: str) -> str:
 
 def code_insee_to_code_dept(code_insee):
     return code_insee[:3] if code_insee.startswith("97") else code_insee[:2]
-
-
-def add_to_sib(user):
-    if settings.SIB_ACTIVE:
-        # Configure API key authorization: api-key
-        configuration = sib_api_v3_sdk.Configuration()
-        configuration.api_key["api-key"] = settings.SIB_API_KEY
-
-        # create an instance of the API class
-        api_instance = sib_api_v3_sdk.ContactsApi(
-            sib_api_v3_sdk.ApiClient(configuration)
-        )
-        create_contact = sib_api_v3_sdk.CreateContact(
-            email=user.email,
-            attributes={
-                "PRENOM": user.first_name,
-                "NOM": user.last_name,
-                "OPT_IN": user.newsletter,
-            },
-            list_ids=[int(settings.SIB_ONBOARDING_LIST)],
-            update_enabled=False,
-        )
-
-        try:
-            # Create a contact
-            api_response = api_instance.create_contact(create_contact)
-            logger.info("User %s added to SiB: %s", user.pk, api_response)
-        except SibApiException as e:
-            logger.exception(e)
 
 
 def get_object_or_none(klass, *args, **kwargs):
