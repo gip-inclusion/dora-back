0539b15972df076af1262da471c18676b322e47d
Frédéric Vergez <fred@ikarius.com>
2023-11-09 15:28:22 +0100

    Configuration de pytest
    - suppression de l'ancien test-runner
    - ajout de configuration dans a minima dans pyproject.toml
    - ajout de conftest.py et de quelques fixtures de compatibilité (db, settings et setup)
    - nettoyage des settings

---
+++ dora/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from model_bakery import baker as baker_
+from rest_framework.test import APIClient
+
+
+@pytest.fixture(autouse=True)
+def setup_test_settings(settings):
+    # La fixture `settings` remets en place les settings en fin de session de test
+    # en remplacement de l'ancien test runner
+    settings.SIB_ACTIVE = False
+    settings.IS_TESTING = True
+    yield
+
+
+@pytest.fixture(autouse=True)
+def _use_db(db):
+    # active automatiquement la gestion de la db
+    # (ce qui n'est pas fait par défaut par pytest)
+    pass
+
+
+@pytest.fixture
+def baker(db):
+    # donne accès à un objet model_bakery
+    return baker_
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
