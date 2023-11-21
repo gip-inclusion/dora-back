8254bbd816e1ddcf41db8b0f612c3a4c553c9682
Frédéric Vergez <fred@ikarius.com>
2023-11-21 17:20:31 +0100

    Ajout de champs sur les tables membres
    - premier admin dans la structure
    - membre inactif depuis 18 mois

--- queries/metabase/views/mb_members_invited.sql
+++ queries/metabase/views/mb_members_invited.sql
@@ -1,53 +1,57 @@
 -- membres d'une structure : 
 -- invité en tant que membre de structure, validé ou non
+-- noqa: disable=LT05
 
 drop table if exists mb_members_invited;
 
 create table mb_members_invited as
 select
-    mu.id               as "ID utilisateur",
-    mu.email            as "E-mail",
-    mu.is_valid         as "E-mail validé",
-    mu.date_joined      as "Date de création",
-    mu.last_login       as "Dernière connexion",
-    ms.name             as "Nom de la structure",
-    ms.dora_url         as "URL Dora",
-    ms.department       as "Département",
-    ss.creation_date    as "Date d'invitation",
-    ss.is_admin         as "Invitation en tant qu'admin",
-    ss.invited_by_admin as "Invitation par un admin",
+    mu.id                                                 as "ID utilisateur",
+    mu.email                                              as "E-mail",
+    mu.is_valid                                           as "E-mail validé",
+    mu.date_joined                                        as "Date de création",
+    mu.last_login                                         as "Dernière connexion",
+    ms.name                                               as "Nom de la structure",
+    ms.dora_url                                           as "URL Dora",
+    ms.department                                         as "Département",
+    ss.creation_date                                      as "Date d'invitation",
+    ss.is_admin                                           as "Invitation en tant qu'admin",
+    ss.invited_by_admin                                   as "Invitation par un admin",
+    (select mu.last_login < now() - interval '18 months') as "Compte inactif",
     (
         select count(*) > 0
         from structures_structuremember as ssm
         inner join mb_user as mu2 on ssm.user_id = mu2.id and mu2.is_valid
         where ssm.is_admin and ssm.structure_id = ms.id
-    )                   as "Structure avec admin actif",
+    )                                                     as "Structure avec admin actif",
     (
         select count(*) from
             structures_structuremember as ss3
         where ss3.structure_id = ss.structure_id and ss3.is_admin
-    )                   as "Nombre d'admins dans la structure",
+    )                                                     as "Nombre d'admins dans la structure",
+    (
+        select count(*) = 0 from
+            structures_structuremember as ss3
+        where ss3.structure_id = ss.structure_id and ss3.is_admin
+    )                                                     as "Premier admin de la structure",
     (
         select count(*)
         from structures_structuremember as ss2
         where
             ss2.user_id = mu.id
-    )                   as "Membre d'autres structures"
+    )                                                     as "Membre d'autres structures"
 from mb_user as mu
 inner join structures_structureputativemember as ss on mu.id = ss.user_id
 left join mb_structure as ms on ss.structure_id = ms.id
 where not mu.is_staff
 order by mu.date_joined desc;
 
--- Indexes et PK
+-- Indexes 
 
-alter table public.mb_members_invited add constraint mb_members_invited_pk primary key ( -- noqa: LT05
-    "ID utilisateur"
-);
 create index mb_members_invited_valide_idx on public.mb_members_invited (
-    "Valide"
+    "E-mail validé"
 );
-create index mb_members_invited_date_joined_idx on public.mb_users_invited (
+create index mb_members_invited_date_joined_idx on public.mb_members_invited (
     "Date de création"
 );
 create index mb_members_invited_dpt_idx on public.mb_members_invited (
