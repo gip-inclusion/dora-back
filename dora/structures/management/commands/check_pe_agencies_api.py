e91f2c9c7bf0b2b1cfb98986f4d7fc7a7f372868
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-12-19 19:03:20 +0100

    WIP

--- dora/structures/management/commands/check_pe_agencies_api.py
+++ dora/structures/management/commands/check_pe_agencies_api.py
@@ -1,3 +1,4 @@
+import csv
 import json
 
 import requests
@@ -58,99 +59,123 @@ class Command(BaseCommand):
         except requests.exceptions.RequestException:
             self.stdout.write("HTTP Request failed")
 
-    def print_error(self, agency, establishment, message):
-        self.stdout.write("###")
-        self.stdout.write(
-            self.style.ERROR(
-                f'{agency["libelleEtendu"]} ({agency["code"]}) : {message}'
-            )
+    def print_error(self, writer, agency, establishment, message):
+        siret = agency.get("siret")
+        addr = agency["adressePrincipale"]
+        writer.writerow(
+            [
+                agency["libelleEtendu"],
+                agency["code"],
+                message,
+                establishment.name if establishment else "",
+                f'=HYPERLINK("https://annuaire-entreprises.data.gouv.fr/etablissement/{siret}")'
+                if siret
+                else "",
+                f"{addr['ligne4']} / {addr['ligne5']} / {addr['ligne6']}",
+                f"{establishment.address1} / {establishment.postal_code} / {establishment.city}"
+                if establishment
+                else "",
+            ]
         )
-        if establishment:
-            self.stdout.write(
-                f'Nom SIRENE: {establishment.name}, nom API: {agency["libelleEtendu"]}'
-            )
-        self.stdout.write(json.dumps(agency))
-        self.stdout.write("")
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.NOTICE("Authentifying to the PE API"))
         pe_access_token = self.get_pe_credentials()["access_token"]
         self.stdout.write(self.style.NOTICE("Getting list of PE agencies"))
         agencies = self.get_pe_agencies(pe_access_token)
-
-        for agency in agencies:
-            try:
-                siret = agency["siret"]
-            except KeyError:
-                if agency["type"] not in ("RPE", "APES"):
-                    self.print_error(agency, None, "SIRET MANQUANT")
-                    continue
-            try:
-                establishment = Establishment.objects.get(siret=siret)
-            except Establishment.DoesNotExist:
-                self.print_error(
-                    agency,
-                    None,
-                    f"SIRET INVALIDE: https://annuaire-entreprises.data.gouv.fr/etablissement/{siret}",
-                )
-                continue
-            # Difficile de comparer les codes postaux, car on a des cedex d'un coté et pas de l'autre
-            # postal_code = agency["adressePrincipale"]["bureauDistributeur"]
-            # if establishment.postal_code != postal_code:
-            #     self.print_error(
-            #         agency,
-            #         f"Code postal incorrect: trouvé {postal_code}, attendu: {establishment.postal_code} — "
-            #         f"https://annuaire-entreprises.data.gouv.fr/etablissement/{siret}",
-            #     )
-            #     print(establishment.name, agency["libelleEtendu"])
-            #     continue
-
-            # Les comparaisons de code insee remontent des résultats intéressants, mais aussi de faux positifs,
-            # car le code insee de l'Etablishment ne provient a priori pas de la base SIRENE elle même,
-            # mais de sa géolocalisation
-            commune_implantation = agency["adressePrincipale"]["communeImplantation"]
-            if establishment.city_code != commune_implantation:
-                self.print_error(
-                    agency,
-                    establishment,
-                    f"Code INSEE probablement incorrect: trouvé {commune_implantation}, attendu: "
-                    f"{establishment.city_code} — https://annuaire-entreprises.data.gouv.fr/etablissement/{siret}",
-                )
-                continue
-
-            lat = agency["adressePrincipale"].get("gpsLat")
-            lon = agency["adressePrincipale"].get("gpsLon")
-            if not lat or not lon:
-                self.print_error(
-                    agency,
-                    establishment,
-                    f"Coordonnées géographiques manquantes: lat: {lat}, lon: {lon}",
-                )
-                continue
-
-            if commune_implantation[:3] in ["975", "977", "978", "986", "987"]:
-                # Les COM ne sont pas dans Admin Express
-                continue
-            city = City.objects.get_from_code(
-                arrdt_to_main_insee_code(commune_implantation)
+        with open("out.csv", "w", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(
+                [
+                    "nom agence",
+                    "code agence",
+                    "erreur",
+                    "nom SIRENE",
+                    "lien annuaire entreprise",
+                    "adresse API",
+                    "adresse INSEE",
+                ]
             )
-            if not city:
-                self.print_error(
-                    agency,
-                    establishment,
-                    f"Commune d'implantation inconnue: {commune_implantation}",
-                )
-                continue
-
-            point = Point(float(lon), float(lat), srid=4326)
-            if not point.within(city.geom):
-                dist = point.distance(city.geom)
-                if dist > 1:
-                    real_city = City.objects.filter(geom__covers=point).first()
+            for agency in agencies:
+                try:
+                    siret = agency["siret"]
+                except KeyError:
+                    if agency["type"] not in ("RPE", "APES"):
+                        self.print_error(writer, agency, None, "SIRET MANQUANT")
+                    continue
+                try:
+                    establishment = Establishment.objects.get(siret=siret)
+                except Establishment.DoesNotExist:
                     self.print_error(
+                        writer,
                         agency,
-                        establishment,
-                        f"Les coordonnées géo ne sont pas dans la commune d'implantation ({commune_implantation}) "
-                        f"mais à {int(dist)} km ({real_city.name}, {real_city.code})",
+                        None,
+                        "SIRET INVALIDE",
                     )
                     continue
+                # Difficile de comparer les codes postaux, car on a des cedex d'un coté et pas de l'autre
+                # postal_code = agency["adressePrincipale"]["bureauDistributeur"]
+                # if establishment.postal_code != postal_code:
+                #     self.print_error(csvfile,
+                #         agency,
+                #         f"Code postal incorrect: trouvé {postal_code}, attendu: {establishment.postal_code} — "
+                #         f"https://annuaire-entreprises.data.gouv.fr/etablissement/{siret}",
+                #     )
+                #     print(establishment.name, agency["libelleEtendu"])
+                #     continue
+
+                # Les comparaisons de code insee remontent des résultats intéressants, mais aussi de faux positifs,
+                # car le code insee de l'Etablishment ne provient a priori pas de la base SIRENE elle même,
+                # mais de sa géolocalisation
+                commune_implantation = agency["adressePrincipale"][
+                    "communeImplantation"
+                ]
+                if establishment.city_code != commune_implantation:
+                    self.print_error(
+                        writer,
+                        agency,
+                        establishment,
+                        f"Code INSEE probablement incorrect: trouvé {commune_implantation}, attendu: "
+                        f"{establishment.city_code}",
+                    )
+                    continue
+
+                lat = agency["adressePrincipale"].get("gpsLat")
+                lon = agency["adressePrincipale"].get("gpsLon")
+                if not lat or not lon:
+                    self.print_error(
+                        writer,
+                        agency,
+                        establishment,
+                        "Coordonnées géographiques manquantes",
+                    )
+                    continue
+
+                if commune_implantation[:3] in ["975", "977", "978", "986", "987"]:
+                    # Les COM ne sont pas dans Admin Express
+                    continue
+                city = City.objects.get_from_code(
+                    arrdt_to_main_insee_code(commune_implantation)
+                )
+                if not city:
+                    self.print_error(
+                        writer,
+                        agency,
+                        establishment,
+                        f"Commune d'implantation inconnue: {commune_implantation}",
+                    )
+                    continue
+
+                point = Point(float(lon), float(lat), srid=4326)
+                if not point.within(city.geom):
+                    dist = point.distance(city.geom)
+                    if dist > 1:
+                        real_city = City.objects.filter(geom__covers=point).first()
+                        self.print_error(
+                            writer,
+                            agency,
+                            establishment,
+                            f"Les coordonnées géo ne sont pas dans la commune d'implantation ({commune_implantation}) "
+                            f"mais à {int(dist)} km ({real_city.name}, {real_city.code})",
+                        )
+                        continue
