d53bd6df3488da1b892ad7a5d553fde47da8fcec
Frédéric Vergez <fred@ikarius.com>
2024-01-22 11:22:53 +0100

    tests: vérification de la suppression de l'utilisateur

--- dora/oidc/tests.py
+++ dora/oidc/tests.py
@@ -10,6 +10,7 @@ from requests_mock import Mocker
 
 from dora.core.test_utils import make_structure, make_user
 from dora.structures.models import StructurePutativeMember
+from dora.users.models import User
 
 from . import OIDCError
 from .utils import updated_ic_user
@@ -63,6 +64,10 @@ def test_updated_ic_user():
     # migration des invitations
     invitation.refresh_from_db()
     assert invitation.user == ic_user
+
+    # l'ancien utilisateur n'existe plus
+    with pytest.raises(User.DoesNotExist):
+        invited_user.refresh_from_db()
 
 
 def test_updated_user_member_of_structure(client_with_cache, settings):
