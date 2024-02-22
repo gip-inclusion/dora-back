bab38ce5329ac651bb248d5dfeb1ed15f8c1e690
Frédéric Vergez <fred@ikarius.com>
2024-02-22 18:20:49 +0100

    metabase: ajout d'une vue absente référencée par un tableau de bord
    Cette vue a du sauter quelque part dans l'historique git ...

---
+++ queries/metabase/03-questions/0031_q_members_invited_invalid.sql
@@ -0,0 +1,30 @@
+-- Question : 
+-- liste des membres invités, avec e-mail non-validé, 
+
+-- noqa: disable=LT05
+
+drop view if exists q_members_invited_invalid;
+
+create view q_members_invited_invalid as
+select
+    "ID utilisateur",
+    "Nom",
+    "Prénom",
+    "E-mail",
+    "E-mail validé",
+    "Date de création",
+    "Dernière connexion",
+    "Nom de la structure",
+    "URL Dora",
+    "SLUG",
+    "Département",
+    "Date d'invitation",
+    "Invitation en tant qu'admin",
+    "Compte inactif",
+    "Nombre d'admins dans la structure",
+    "Premier admin de la structure",
+    "Membre d'autres structures"
+from mb_putative_members
+where not "E-mail validé" and "Invitation par un admin";
+
+comment on view q_members_invited_invalid is 'Liste des membres invités en attente de rattachement : e-mail non-validé';
