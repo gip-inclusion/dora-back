40e548edda5f60a95f04c8fde8ac3c895debc097
Frédéric Vergez <fred@ikarius.com>
2024-02-26 15:38:04 +0100

    Notifications : utilisateurs actifs sans structure (CAT 4)
    Envoi de notifications pour les utilisateurs non rattachés à une structure
    et sans rattachement en attente.
    
    4 notifications, puis suppression du compte.

--- dora/users/emails.py
+++ dora/users/emails.py
@@ -23,3 +23,35 @@ def send_invitation_reminder(user, structure):
         user.email,
         mjml2html(render_to_string("invitation_reminder.mjml", context)),
     )
+
+
+def send_user_without_structure_notification(user, deletion=False):
+    # même notification et contexte mais template différent si dernier rappel
+    cta_link = furl(settings.FRONTEND_URL) / "auth" / "rattachement"
+    cta_link.add(
+        {
+            "login_hint": iri_to_uri(user.email),
+            "mtm_campaign": "MailsTransactionnels",
+            "mtm_keyword": "InscritSansStructure",
+        }
+    )
+    context = {
+        "user": user,
+        "cta_link": cta_link.url,
+        "with_legal_info": True,
+    }
+
+    send_mail(
+        "Dernier rappel avant suppression"
+        if deletion
+        else "Rappel : Identifiez votre structure sur DORA",
+        user.email,
+        mjml2html(
+            render_to_string(
+                "notification_user_without_structure_deletion.mjml"
+                if deletion
+                else "notification_user_without_structure.mjml",
+                context,
+            )
+        ),
+    )
