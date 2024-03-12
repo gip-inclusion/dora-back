a069f8333d369644fc51d26057f2df8e23a218c5
Frédéric Vergez <fred@ikarius.com>
2024-03-12 06:47:06 +0100

    Ajout d'un validateur pour les numéros SIREN et SIRET
    Un peu plus complet pour les SIRET

--- dora/core/validators.py
+++ dora/core/validators.py
@@ -1,6 +1,10 @@
+from itertools import chain
+
 from django.core.exceptions import ValidationError
 from osm_time import ParseException
 from osm_time.opening_hours import OpeningHours
+
+from .constants import SIREN_LA_POSTE
 
 
 # From: https://github.com/betagouv/itou/blob/master/itou/utils/validators.py
@@ -24,3 +28,47 @@ def validate_accesslibre_url(url):
 def validate_safir(safir):
     if not safir.isdigit() or len(safir) != 5:
         raise ValidationError("Le code SAFIR doit être composé de 5 chiffres.")
+
+
+def _validation_key(siren_or_siret):
+    # calcul de validité du SIREN et SIRET
+    # voir : https://www.sirene.fr/static-resources/doc/lettres/lettre-16-novembre-2013.pdf
+
+    tmp = siren_or_siret[::-1]
+    even = [int(d) for d in tmp[::2]]
+    odd = [str(2 * int(d)) for d in tmp[1::2]]
+
+    # décompose les nombres supérieurs à 9 en chiffres
+    odd = list(map(int, chain(*odd)))
+
+    # pour être valide, la clé doit être un multiple de 10
+    return sum([*even, *odd])
+
+
+def validate_siren(siren):
+    if not siren.isdigit() or len(siren) != 9:
+        raise ValidationError("Le numéro SIREN doit être composé de 9 chiffres.")
+
+    key = _validation_key(siren)
+
+    if key % 10:
+        raise ValidationError(f"Le numéro SIREN est incorrect (clé: {key}).")
+
+
+def validate_full_siret(siret):
+    validate_siret(siret)
+
+    # les SIRET peuvent être validés comme les SIREN,
+    # sauf pour les agences de la Poste
+
+    if siret.startswith(SIREN_LA_POSTE):
+        # cas particulier de La Poste : la somme des chiffres du SIRET doit être un multiple de 5
+        key = sum([int(d) for d in siret])
+        if key % 5:
+            raise ValidationError(f"Le numéro SIRET est incorrect (clé: {key}).")
+        return
+
+    key = _validation_key(siret)
+
+    if key % 10:
+        raise ValidationError(f"Le numéro SIRET est incorrect (clé: {key}).")
