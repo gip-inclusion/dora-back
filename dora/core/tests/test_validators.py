fa09b086054874f0fc90df4d52834e4a6656eb54
ikarius <fred@ikarius.com>
2024-03-14 10:13:53 +0100

    Modification du script de l'import de structures (#246)
    Pour valider un peu mieux ce qui se passe sur l'import des structures
    (un peu galère le 971)
    - ajout d'un validateur pour les numéros SIREN
    - correction d'une erreur de parsing
    - ...

---
+++ dora/core/tests/test_validators.py
@@ -0,0 +1,38 @@
+import pytest
+from django.core.exceptions import ValidationError
+
+from ..constants import SIREN_LA_POSTE, SIREN_POLE_EMPLOI
+from ..validators import validate_full_siret, validate_siren
+
+
+def test_validate_siren():
+    # SIREN de l'INSEE
+    validate_siren("120027016")
+
+    # SIREN de La Poste (attention aux SIRET des agences)
+    validate_siren(SIREN_LA_POSTE)
+
+    # SIREN FT / PE
+    validate_siren(SIREN_POLE_EMPLOI)
+
+    with pytest.raises(ValidationError, match="Le numéro SIREN est incorrect"):
+        # SIREN exemple
+        validate_siren("123456789")
+
+
+def test_validate_siret_full():
+    # agence FT Paris 18
+    validate_full_siret("13000548121800")
+
+    with pytest.raises(ValidationError, match="Le numéro SIRET est incorrect"):
+        # un SIRET incorrect basé sur le SIREN FT / PE
+        validate_full_siret(SIREN_POLE_EMPLOI + "12345")
+
+
+def test_validate_siret_full_la_poste():
+    # Agence La Poste - Paris 1er Les Halles
+    validate_full_siret(SIREN_LA_POSTE + "37187")
+
+    with pytest.raises(ValidationError, match="Le numéro SIRET est incorrect"):
+        # un SIRET incorrect basé sur le SIREN La Poste
+        validate_full_siret(SIREN_LA_POSTE + "12345")
