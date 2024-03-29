4ff045a8ed2b437eb0c28287b3efb7b9d767ab9e
Gérald Gounot <gerald@gounot.eu>
2024-03-29 10:39:42 +0100

    join_structure: ajout vérification adresse francetravail.fr

--- dora/rest_auth/views.py
+++ dora/rest_auth/views.py
@@ -153,11 +153,12 @@ def join_structure(request):
         and user.email.split("@")[1]
         not in (
             "pole-emploi.fr",
+            "francetravail.fr",
             "beta.gouv.fr",
         )
     ):
         raise exceptions.PermissionDenied(
-            "Seuls les agents Pôle emploi peuvent se rattacher à une agence Pôle emploi"
+            "Seuls les agents France Travail peuvent se rattacher à une agence France Travail"
         )
 
     if establishment:
