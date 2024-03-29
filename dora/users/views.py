6bf0cc68844f8ff9acf389db8d0818882fa88f7d
Gérald Gounot <gerald@gounot.eu>
2024-03-29 17:22:01 +0100

    transforme update_main_activity en update_user_profile pour gérer plusieurs champs

--- dora/users/views.py
+++ dora/users/views.py
@@ -5,18 +5,21 @@ from rest_framework.response import Response
 from .models import User
 
 
-class UserMainActivitySerializer(serializers.ModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["main_activity"]
+        fields = ["main_activity", "discovery_method", "discovery_method_other"]
+
+    def validate(self, attrs):
+        if 'main_activity' not in attrs and not self.instance.main_activity:
+            raise serializers.ValidationError("Le champ « Activité principale » est requis")
+        return attrs
 
 
 @api_view(["POST"])
 @permission_classes([permissions.IsAuthenticated])
-def update_main_activity(request):
-    serializer = UserMainActivitySerializer(data=request.data)
+def update_user_profile(request):
+    serializer = UserProfileSerializer(request.user, data=request.data, partial=True)
     serializer.is_valid(raise_exception=True)
-
-    request.user.main_activity = serializer.validated_data["main_activity"]
-    request.user.save()
+    serializer.save()
     return Response(status=204)
