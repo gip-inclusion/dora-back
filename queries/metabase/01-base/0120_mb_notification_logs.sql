c171f5d4224fff38e36d3a5532fde09b9362638f
ikarius <fred@ikarius.com>
2024-04-29 17:43:32 +0200

    Ajout de logs pour les notifications (#276)
    Pour effectuer un suivi cumulatif des envois de notifications et tracer
    les suppressions d'utilisateurs.
    
    Ah oui, ça dépends de :
    https://github.com/gip-inclusion/dora-back/pull/275

---
+++ queries/metabase/01-base/0120_mb_notification_logs.sql
@@ -0,0 +1,29 @@
+-- Historique des traitements de tâches de notification
+
+-- noqa: disable=LT05
+
+drop table if exists mb_notification_logs cascade;
+
+create table mb_notification_logs as
+select
+    created_at                 as "date_creation",
+    payload ->> 'taskType'     as "tache",
+    payload ->> 'nbCandidates' as "nb_bandidats",
+    payload ->> 'nbProcessed'  as "nb_traitees",
+    payload ->> 'nbObsolete'   as "nb_obsoletes",
+    payload ->> 'nbErrors'     as "nb_erreurs"
+from logs_actionlog
+where
+    msg like 'process_notification_tasks:%'
+    -- log level 20 => INFO
+    and level = 20;
+
+
+create index mb_notification_logs_date_creation_idx on public.mb_notification_logs (
+    "date_creation"
+);
+create index mb_notification_logs_tache_idx on public.mb_notification_logs (
+    "tache"
+);
+
+comment on table mb_notification_logs is 'Historique des tâches de traitement de notification';
