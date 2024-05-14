aad285817d5fe7093225a557d3e2572caf20030f
ikarius <fred@ikarius.com>
2024-05-14 21:59:14 +0200

    fix: utilisation d'un tag unique pour les e-mails de notification (#291)
    Les tags d'e-mails de notifs étaient soit absents, soit spécifiques.
    Maintenant harmonisé avec le tag unique : "notification".
    Si besoin de plus d'information, se référer aux logs (ou en créer).

--- dora/users/emails.py
+++ dora/users/emails.py
@@ -33,6 +33,7 @@ def send_invitation_reminder(user, structure, notification=False):
         f"Rappel : Acceptez l'invitation à rejoindre {structure.name} sur DORA",
         user.email,
         mjml2html(render_to_string("invitation_reminder.mjml", context)),
+        tags=["notification"],
     )
 
 
@@ -65,6 +66,7 @@ def send_user_without_structure_notification(user, deletion=False):
                 context,
             )
         ),
+        tags=["notification"],
     )
 
 
@@ -87,4 +89,5 @@ def send_account_deletion_notification(user):
         "DORA - Suppression prochaine de votre compte",
         user.email,
         mjml2html(render_to_string("notification_account_deletion.mjml", context)),
+        tags=["notification"],
     )
