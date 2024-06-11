b2d81fa520772cc1dc63f3258e5f05a6da5cf40c
Gérald Gounot <gerald@gounot.eu>
2024-06-11 18:04:16 +0200

    Ajout migration modes d'orientation

---
+++ dora/services/migrations/0105_update_orientation_modes.py
@@ -0,0 +1,126 @@
+from django.conf import settings
+from django.db import migrations
+from django.db.models import Q
+
+from dora.services.enums import ServiceStatus
+
+
+def is_orientable(service):
+    structure_blacklisted = False
+    for siren in settings.ORIENTATION_SIRENE_BLACKLIST:
+        if service.structure.siret and service.structure.siret.startswith(siren):
+            structure_blacklisted = True
+            break
+
+    return (
+        service.status == ServiceStatus.PUBLISHED
+        and not service.structure.disable_orientation_form
+        and not structure_blacklisted
+        and service.contact_email
+        and (
+            service.coach_orientation_modes.filter(
+                Q(value="envoyer-courriel") | Q(value="envoyer-fiche-prescription")
+            ).exists()
+            or service.beneficiaries_access_modes.filter(
+                value="envoyer-courriel"
+            ).exists()
+        )
+    )
+
+
+def replace_envoyer_formulaire_with_envoyer_courriel(CoachOrientationMode):
+    envoyer_formulaire = CoachOrientationMode.objects.get(value="envoyer-formulaire")
+    envoyer_courriel = CoachOrientationMode.objects.get(value="envoyer-courriel")
+
+    services = envoyer_formulaire.service_set.all()
+
+    for service in services:
+        service.coach_orientation_modes.remove(envoyer_formulaire)
+        service.coach_orientation_modes.add(envoyer_courriel)
+
+    envoyer_formulaire.delete()
+
+
+def add_new_coach_orientation_modes(CoachOrientationMode):
+    CoachOrientationMode(
+        value="formulaire-dora",
+        label="Via le formulaire DORA",
+    ).save()
+
+    CoachOrientationMode(
+        value="formulaire-externe",
+        label="Via notre propre formulaire/site internet",
+    ).save()
+
+
+def reword_coach_orientation_mode_labels(CoachOrientationMode):
+    CoachOrientationMode.objects.filter(value="envoyer-courriel").update(
+        label="Nous envoyer un e-mail"
+    )
+    CoachOrientationMode.objects.filter(value="envoyer-fiche-prescription").update(
+        label="Nous envoyer un e-mail avec une fiche de prescription"
+    )
+    CoachOrientationMode.objects.filter(value="telephoner").update(
+        label="Nous téléphoner"
+    )
+    CoachOrientationMode.objects.filter(value="autre").update(
+        label="Autre modalité (préciser)"
+    )
+
+
+def set_formulaire_dora_for_orientable_services(CoachOrientationMode, Service):
+    formulaire_dora = CoachOrientationMode.objects.get(value="formulaire-dora")
+
+    for service in Service.objects.all():
+        if is_orientable(service):
+            service.coach_orientation_modes.add(formulaire_dora)
+
+
+def add_new_beneficiary_access_modes(BeneficiaryAccessMode):
+    BeneficiaryAccessMode(
+        value="professionnel",
+        label="Se faire orienter par un professionnel",
+    ).save()
+
+    BeneficiaryAccessMode(
+        value="formulaire-externe",
+        label="Via notre propre formulaire/site internet",
+    ).save()
+
+
+def reword_beneficiary_access_mode_labels(BeneficiaryAccessMode):
+    BeneficiaryAccessMode.objects.filter(value="envoyer-courriel").update(
+        label="Nous envoyer un e-mail"
+    )
+    BeneficiaryAccessMode.objects.filter(value="telephoner").update(
+        label="Nous téléphoner"
+    )
+    BeneficiaryAccessMode.objects.filter(value="autre").update(
+        label="Autre modalité (préciser)"
+    )
+
+
+def update_orientation_modes(apps, schema_editor):
+    Service = apps.get_model("services", "Service")
+    CoachOrientationMode = apps.get_model("services", "CoachOrientationMode")
+    BeneficiaryAccessMode = apps.get_model("services", "BeneficiaryAccessMode")
+
+    # Modalités accompagnateur
+    add_new_coach_orientation_modes(CoachOrientationMode)
+    reword_coach_orientation_mode_labels(CoachOrientationMode)
+    set_formulaire_dora_for_orientable_services(CoachOrientationMode, Service)
+    replace_envoyer_formulaire_with_envoyer_courriel(CoachOrientationMode)
+
+    # Modalités bénéficiaire
+    add_new_beneficiary_access_modes(BeneficiaryAccessMode)
+    reword_beneficiary_access_mode_labels(BeneficiaryAccessMode)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("services", "0104_service_appointment_link"),
+    ]
+
+    operations = [
+        migrations.RunPython(update_orientation_modes),
+    ]
