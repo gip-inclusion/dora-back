8e8f1f87226188aaddd1c019d13710648e5744ae
Frédéric Vergez <fred@ikarius.com>
2024-08-08 12:14:49 +0200

    sql: ajout de vues permettant de quantifier l'activité des utilisateurs
    Essentiellement pour la définition des "power-users" sans encore
    l'utilisation des sessions utilisateurs.

---
+++ queries/stats/01-utilisateurs/0010_v_searches_for_user.sql
@@ -0,0 +1,28 @@
+-- Décompte des recherches effectuées par des utilisateurs connectés :
+-- non-gestionnaire, 
+-- non-membre de l'équipe, 
+-- et offreur seulement. 
+
+drop view v_searches_for_user;
+
+create or replace view v_searches_for_user as
+select
+    anonymous_user_hash,
+    date_part('doy', date)   as jour,
+    date_part('week', date)  as semaine,
+    date_part('month', date) as mois,
+    date_part('year', date)  as annee,
+    count(*)                 as nb
+from stats_searchview
+where
+    user_kind != 'offreur'
+    and not is_manager
+    and not is_staff
+    and is_logged
+group by
+    anonymous_user_hash,
+    jour,
+    semaine,
+    mois,
+    annee
+order by annee asc, jour asc, nb desc;
