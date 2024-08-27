4eba0a73ffbb8bdd564ce25171842c9b504a29c1
ikarius <fred@ikarius.com>
2024-08-27 09:31:09 +0200

    fix: correction des vues par structure (vue SQL) (#368)
    Les parties de dates à extraire étaient incorrectes (en français).

--- queries/stats/01-utilisateurs/0012_v_structure_views_for_user.sql
+++ queries/stats/01-utilisateurs/0012_v_structure_views_for_user.sql
@@ -9,11 +9,11 @@ drop view v_structure_views_for_user;
 create or replace view v_structure_views_for_user as
 select
     anonymous_user_hash,
-    date_part('doy', date)     as jour,
-    date_part('semaine', date) as semaine,
-    date_part('mois', date)    as mois,
-    date_part('annee', date)   as annee,
-    count(*)                   as nb
+    date_part('doy', date)   as jour,
+    date_part('week', date)  as semaine,
+    date_part('month', date) as mois,
+    date_part('year', date)  as annee,
+    count(*)                 as nb
 from stats_structureview as ss
 where
     user_kind != 'offreur'
