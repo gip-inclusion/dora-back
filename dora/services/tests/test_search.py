0b087ceac2409c4c8fc067a38bf729baec80e82a
Gérald Gounot <gerald@gounot.eu>
2024-09-25 17:42:03 +0200

    test_search.py: ajout d'un test du filtrage des services Action Logement

--- dora/services/tests/test_search.py
+++ dora/services/tests/test_search.py
@@ -1,8 +1,15 @@
+from unittest import mock
+
 import pytest
 from model_bakery import baker
 
 from dora.admin_express.models import AdminDivisionType
-from dora.core.test_utils import make_service, make_structure, make_user
+from dora.core.test_utils import (
+    make_di_service,
+    make_service,
+    make_structure,
+    make_user,
+)
 from dora.services.enums import ServiceStatus
 
 
@@ -52,3 +59,46 @@ def test_search_services_with_orphan_structure(
     [found] = response.data["services"]
 
     assert found["slug"] == orphan_service.slug
+
+
+def search_services_excludes_some_action_logement_results_mock(*args, **kwargs):
+    return [
+        make_di_service(
+            service={
+                "thematiques": [
+                    "logement-hebergement",
+                    "logement-hebergement--aides-financieres-investissement-locatif",
+                    "logement-hebergement--besoin-dadapter-mon-logement",
+                ]
+            }
+        ),
+        make_di_service(
+            service={
+                "thematiques": [
+                    "logement-hebergement",
+                    "logement-hebergement--besoin-dadapter-mon-logement",
+                ]
+            }
+        ),
+    ]
+
+
+def test_search_services_excludes_some_action_logement_results(api_client):
+    # Le service ayant la thématique logement-hebergement--aides-financieres-investissement-locatif
+    # ne doit pas être retourné
+
+    # le paramètre `city` est nécessaire a minima
+    city = baker.make("City")
+
+    with mock.patch("dora.data_inclusion.client.di_client_factory") as mock_di_client:
+        mock_di_client.return_value.search_services = (
+            search_services_excludes_some_action_logement_results_mock
+        )
+
+        response = api_client.get(f"/search/?city={city.code}")
+
+        assert response.status_code == 200
+
+        assert (
+            len(response.data["services"]) == 1
+        ), "un seul service devrait être retourné"
