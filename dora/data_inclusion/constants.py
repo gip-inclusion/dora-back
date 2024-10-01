cefc1dc427d9f175cb86667e38516515de795e13
Gérald Gounot <gerald@gounot.eu>
2024-10-01 17:50:38 +0200

    data_inclusion: ajout d'un dictionnaire de correspondances de thématiques

---
+++ dora/data_inclusion/constants.py
@@ -0,0 +1,5 @@
+THEMATIQUES_MAPPING = {
+    "logement-hebergement--etre-accompagne-dans-son-projet-accession": "logement-hebergement--etre-accompagne-pour-se-loger",
+    "logement-hebergement--etre-accompagne-en cas-de-difficultes-financieres": "logement-hebergement--gerer-son-budget",
+    "logement-hebergement--financer-son-projet-travaux": "logement-hebergement--autre",
+}
